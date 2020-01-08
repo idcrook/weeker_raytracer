5fbc263a55083cfdb707ff922b5a7b9ba50a9f7e
David Crook <idcrook@users.noreply.github.com>
2020-01-08 12:37:40 -0700

    done with ch3 (added vec3 data structure)

--- src/program.cpp
+++ src/program.cpp
@@ -1,4 +1,8 @@
+#include "vec3.h"
+
 #include <iostream>
+
+// ./build/apps/program > output/ch3a.ppm
 
 int main() {
     int nx = 200;
@@ -6,12 +10,11 @@ int main() {
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
     for (int j = ny-1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
-            float r = float(i) / float(nx);
-            float g = float(j) / float(ny);
-            float b = 0.2;
-            int ir = int(255.99*r);
-            int ig = int(255.99*g);
-            int ib = int(255.99*b);
+            vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+            int ir = int(255.99*col[0]);
+            int ig = int(255.99*col[1]);
+            int ib = int(255.99*col[2]);
+
             std::cout << ir << " " << ig << " " << ib << "\n";
         }
     }
