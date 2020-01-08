1b2be216497e0708dae76d400aad83fe31fedb06
David Crook <idcrook@users.noreply.github.com>
2020-01-08 12:50:48 -0700

    ch4 completed

--- src/program.cpp
+++ src/program.cpp
@@ -1,16 +1,31 @@
-#include "vec3.h"
+#include "ray.h"
 
 #include <iostream>
 
-// ./build/apps/program > output/ch3a.ppm
+vec3 color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    // scales y [-1, 1] -> t [0, 1]
+    float t = 0.5*(unit_direction.y() + 1.0);
+    // linear interpolate from white (t = 0) to light blue (t = 1)
+    return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+}
+
+// ./build/apps/program > output/ch4a.ppm
 
 int main() {
     int nx = 200;
     int ny = 100;
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+    vec3 lower_left_corner(-2.0, -1.0, -1.0);
+    vec3 horizontal(4.0, 0.0, 0.0);
+    vec3 vertical(0.0, 2.0, 0.0);
+    vec3 origin(0.0, 0.0, 0.0);
     for (int j = ny-1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
-            vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+            float u = float(i) / float(nx);
+            float v = float(j) / float(ny);
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+            vec3 col = color(r);
             int ir = int(255.99*col[0]);
             int ig = int(255.99*col[1]);
             int ib = int(255.99*col[2]);
