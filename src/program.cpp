19beac3cf8d36c6b0d4aca400d4f6fbc0dd78d90
David Crook <idcrook@users.noreply.github.com>
2020-01-09 12:17:00 -0700

    ch 7 anti-aliasing complete
    subpixel random sampling

--- src/program.cpp
+++ src/program.cpp
@@ -1,6 +1,8 @@
 #include "float.h"
 #include "hittable_list.h"
 #include "sphere.h"
+#include "camera.h"
+#include "random.h"
 
 #include <iostream>
 
@@ -15,16 +17,13 @@ vec3 color(const ray& r, hittable *world) {
         return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
     }
 }
-// ./build/apps/program > output/ch6b.ppm
+// ./build/apps/program > output/ch7a.ppm
 
 int main() {
     int nx = 200;
     int ny = 100;
+    int ns = 100;
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-    vec3 lower_left_corner(-2.0, -1.0, -1.0);
-    vec3 horizontal(4.0, 0.0, 0.0);
-    vec3 vertical(0.0, 2.0, 0.0);
-    vec3 origin(0.0, 0.0, 0.0);
 
     // create hittable list
     hittable *list[2];
@@ -32,14 +31,19 @@ int main() {
     list[1] = new sphere(vec3(0,-100.5,-1), 100);
     hittable *world = new hittable_list(list,2);
 
+    camera cam;
+
     for (int j = ny-1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
-            float u = float(i) / float(nx);
-            float v = float(j) / float(ny);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
 
-            //vec3 p = r.point_at_parameter(2.0);
-            vec3 col = color(r, world);
+            vec3 col(0, 0, 0);
+            for (int s = 0; s < ns; s++) {
+                float u = float(i + random_double()) / float(nx);
+                float v = float(j + random_double()) / float(ny);
+                ray r = cam.get_ray(u, v);
+                col += color(r, world);
+            }
+            col /= float(ns);
 
             int ir = int(255.99*col[0]);
             int ig = int(255.99*col[1]);
