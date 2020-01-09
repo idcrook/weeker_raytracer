5e7c3e2809baf0db49410db7032f3d820fe6f3d6
David Crook <idcrook@users.noreply.github.com>
2020-01-08 19:55:59 -0700

    ch5 complete

--- src/program.cpp
+++ src/program.cpp
@@ -2,7 +2,21 @@
 
 #include <iostream>
 
+bool hit_sphere(const vec3& center, float radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    // p(t) = A + B*t , circle origin at C (center), radius R (radius)
+    // t^2 * dot(B,B) + 2*t*dot(B, A-C) + dot(A-C, A-C) - R^2 = 0
+    // a*t^2 + b*t + c
+    float a = dot(r.direction(), r.direction());
+    float b = 2.0 * dot(oc, r.direction());
+    float c = dot(oc, oc) - radius*radius;
+    float discriminant = b*b - 4*a*c;
+    return (discriminant > 0);
+}
+
 vec3 color(const ray& r) {
+    if (hit_sphere(vec3(0,0,-2), 0.7071, r))
+        return vec3(1, 0, 0);
     vec3 unit_direction = unit_vector(r.direction());
     // scales y [-1, 1] -> t [0, 1]
     float t = 0.5*(unit_direction.y() + 1.0);
@@ -10,7 +24,7 @@ vec3 color(const ray& r) {
     return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
 }
 
-// ./build/apps/program > output/ch4a.ppm
+// ./build/apps/program > output/ch5a.ppm
 
 int main() {
     int nx = 200;
