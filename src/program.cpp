80b86d66f0e8e7db89b269fb96edf31d597f803d
David Crook <idcrook@users.noreply.github.com>
2020-01-08 20:19:42 -0700

    ch6, part 1

--- src/program.cpp
+++ src/program.cpp
@@ -2,7 +2,8 @@
 
 #include <iostream>
 
-bool hit_sphere(const vec3& center, float radius, const ray& r) {
+
+float hit_sphere(const vec3& center, float radius, const ray& r) {
     vec3 oc = r.origin() - center;
     // p(t) = A + B*t , circle origin at C (center), radius R (radius)
     // t^2 * dot(B,B) + 2*t*dot(B, A-C) + dot(A-C, A-C) - R^2 = 0
@@ -11,20 +12,30 @@ bool hit_sphere(const vec3& center, float radius, const ray& r) {
     float b = 2.0 * dot(oc, r.direction());
     float c = dot(oc, oc) - radius*radius;
     float discriminant = b*b - 4*a*c;
-    return (discriminant > 0);
+    if (discriminant < 0) {
+      return -1.0;
+    } else {
+      // use nearest quadratic solution (-z is the viewport direction)
+      return (-b - sqrt(discriminant)) / (2.0*a);
+    }
 }
 
 vec3 color(const ray& r) {
-    if (hit_sphere(vec3(0,0,-2), 0.7071, r))
-        return vec3(1, 0, 0);
-    vec3 unit_direction = unit_vector(r.direction());
-    // scales y [-1, 1] -> t [0, 1]
-    float t = 0.5*(unit_direction.y() + 1.0);
-    // linear interpolate from white (t = 0) to light blue (t = 1)
-    return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+  float t = hit_sphere(vec3(0,0,-1), 0.5, r);
+  if (t>0) {
+    // compute a unit normal for the sphere origin: (0,0,-1) hit
+    vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0,0,-1));
+    // scale [-1,1] -> [0,1] and use that as colorspace
+    return 0.5*vec3(N.x()+1, N.y()+1, N.z()+1);
+  }
+  vec3 unit_direction = unit_vector(r.direction());
+  // scales y [-1, 1] -> t [0, 1]
+  t = 0.5*(unit_direction.y() + 1.0);
+  // linear interpolate from white (t = 0) to light blue (t = 1)
+  return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
 }
 
-// ./build/apps/program > output/ch5a.ppm
+// ./build/apps/program > output/ch6a.ppm
 
 int main() {
     int nx = 200;
