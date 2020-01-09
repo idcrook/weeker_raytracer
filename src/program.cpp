17b7243fab62556f4e32821c76fbfa1a65247652
David Crook <idcrook@users.noreply.github.com>
2020-01-09 12:24:04 -0700

    update indentation; changed sub-pixel sample ranges

--- src/program.cpp
+++ src/program.cpp
@@ -7,49 +7,49 @@
 #include <iostream>
 
 vec3 color(const ray& r, hittable *world) {
-    hit_record rec;
-    if (world->hit(r, 0.0, MAXFLOAT, rec)) {
-        return 0.5*vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
-    }
-    else {
-        vec3 unit_direction = unit_vector(r.direction());
-        float t = 0.5*(unit_direction.y() + 1.0);
-        return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
-    }
+  hit_record rec;
+  if (world->hit(r, 0.0, MAXFLOAT, rec)) {
+    return 0.5*vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
+  }
+  else {
+    vec3 unit_direction = unit_vector(r.direction());
+    float t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+  }
 }
 // ./build/apps/program > output/ch7a.ppm
 
 int main() {
-    int nx = 200;
-    int ny = 100;
-    int ns = 100;
-    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+  int nx = 200;
+  int ny = 100;
+  int ns = 100;
+  std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
-    // create hittable list
-    hittable *list[2];
-    list[0] = new sphere(vec3(0,0,-1), 0.5);
-    list[1] = new sphere(vec3(0,-100.5,-1), 100);
-    hittable *world = new hittable_list(list,2);
+  // create hittable list
+  hittable *list[2];
+  list[0] = new sphere(vec3(0,0,-1), 0.5);
+  list[1] = new sphere(vec3(0,-100.5,-1), 100);
+  hittable *world = new hittable_list(list,2);
 
-    camera cam;
+  camera cam;
 
-    for (int j = ny-1; j >= 0; j--) {
-        for (int i = 0; i < nx; i++) {
+  for (int j = ny-1; j >= 0; j--) {
+    for (int i = 0; i < nx; i++) {
 
-            vec3 col(0, 0, 0);
-            for (int s = 0; s < ns; s++) {
-                float u = float(i + random_double()) / float(nx);
-                float v = float(j + random_double()) / float(ny);
-                ray r = cam.get_ray(u, v);
-                col += color(r, world);
-            }
-            col /= float(ns);
+      vec3 col(0, 0, 0);
+      for (int s = 0; s < ns; s++) {
+        float u = float(i + random_double() - (1.0/2.0)) / float(nx);
+        float v = float(j + random_double() - (1.0/2.0)) / float(ny);
+        ray r = cam.get_ray(u, v);
+        col += color(r, world);
+      }
+      col /= float(ns);
 
-            int ir = int(255.99*col[0]);
-            int ig = int(255.99*col[1]);
-            int ib = int(255.99*col[2]);
+      int ir = int(255.99*col[0]);
+      int ig = int(255.99*col[1]);
+      int ib = int(255.99*col[2]);
 
-            std::cout << ir << " " << ig << " " << ib << "\n";
-        }
+      std::cout << ir << " " << ig << " " << ib << "\n";
     }
+  }
 }
