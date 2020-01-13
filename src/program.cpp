c27f58eb8e8c237b9a138a23dc4ce3dc6602d652
David Crook <idcrook@users.noreply.github.com>
2020-01-12 19:38:28 -0700

    add moving sphere
     - add command line interface for HQ vs non-HQ output

--- src/program.cpp
+++ src/program.cpp
@@ -26,9 +26,58 @@ vec3 color(const ray& r, hittable *world, int depth) {
     return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
   }
 }
-// ./build/apps/program > output/ch13a.ppm
+// ./build/apps/program > output/ch2a.ppm
 
 hittable *random_scene() {
+  int n = 50000;
+  hittable **list = new hittable*[n+1];
+  list[0] =  new sphere(vec3(0,-1000,0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+  int i = 1;
+  for (int a = -10; a < 10; a++) {
+    for (int b = -10; b < 10; b++) {
+      float choose_mat = random_double();
+      vec3 center(a+0.9*random_double(),0.2,b+0.9*random_double());
+      if ((center-vec3(4,0.2,0)).length() > 0.9) {
+        if (choose_mat < 0.8) {  // diffuse
+          list[i++] = new
+            moving_sphere(
+                          center,
+                          center+vec3(0, 0.5*random_double(), 0),
+                          0.0, 1.0, 0.2,
+                          new lambertian(
+                                         vec3(random_double()*random_double(),
+                                              random_double()*random_double(),
+                                              random_double()*random_double())
+                                         )
+                          );
+        }
+        else if (choose_mat < 0.95) { // metal
+          list[i++] = new
+            sphere(
+                   center, 0.2,
+                   new metal(
+                             vec3(0.5*(1 + random_double()),
+                                  0.5*(1 + random_double()),
+                                  0.5*(1 + random_double())),
+                             0.5*random_double()
+                             )
+                   );
+        }
+        else {  // glass
+          list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+        }
+      }
+    }
+  }
+
+  list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+  list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+  list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+
+  return new hittable_list(list,i);
+}
+
+hittable *random_scene0() {
   int n = 500;
   // oversized container
   hittable **list = new hittable*[2*n+1];
@@ -70,16 +119,34 @@ hittable *random_scene() {
   return new hittable_list(list,i);
 }
 
-int main() {
-  int nx = 200;
-  int ny = 100;
-  int ns = 10;
-  // int nx = 200;
-  // int ny = 100;
-  // int ns = 100;
-  // int nx = 1200;
-  // int ny = 800;
-  // int ns = 10;
+//int main() {
+int main (int argc, char** argv) {
+
+  // default value for higher resolution render
+  bool HIGH_QUALITY_RENDER = !true;
+
+  // handle command line arguments
+  if (argc >= 2) {
+    // first command line argument is "HQ"
+    if (std::string(argv[1]) == "HQ") {
+      HIGH_QUALITY_RENDER = true;
+    }
+  }
+
+  int nx, ny, ns;
+
+  if (! HIGH_QUALITY_RENDER) {
+    nx = 200;
+    ny = 100;
+    ns = 10;
+  } else {
+    nx = 1200;
+    ny = 800;
+    ns = 10;
+  }
+
+  std::cerr << "Total Scanlines: " << ny << std::endl;
+
   std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
   hittable *world = random_scene();
