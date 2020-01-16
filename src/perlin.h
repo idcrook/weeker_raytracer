c158f7571111662ed7cacf2a25147d9eee60b43a
David Crook <idcrook@users.noreply.github.com>
2020-01-15 17:09:07 -0700

    ch5: perlin interpolate; color output broken (large negative numbers)

--- src/perlin.h
+++ src/perlin.h
@@ -4,17 +4,23 @@
 #include "random.h"
 #include "vec3.h"
 
-inline float trilinear_interp(float c[2][2][2], float u, float v, float w) {
+
+inline float perlin_interp(vec3 c[2][2][2], float u, float v, float w) {
+  float uu = u*u*(3-2*u);
+  float vv = v*v*(3-2*v);
+  float ww = w*w*(3-2*w);
   float accum = 0;
   for (int i=0; i < 2; i++)
     for (int j=0; j < 2; j++)
-      for (int k=0; k < 2; k++)
-        accum += (i*u + (1-i)*(1-u))*
-          (j*v + (1-j)*(1-v))*
-          (k*w + (1-k)*(1-w))*c[i][j][k];
-
+      for (int k=0; k < 2; k++) {
+        vec3 weight_v(u-i, v-j, w-k);
+        accum += (i*uu + (1-i)*(1-uu))*
+          (j*vv + (1-j)*(1-vv))*
+          (k*ww + (1-k)*(1-ww))*dot(c[i][j][k], weight_v);
+      }
   return accum;
 }
+
 
 class perlin {
 public:
@@ -22,34 +28,35 @@ public:
     float u = p.x() - floor(p.x());
     float v = p.y() - floor(p.y());
     float w = p.z() - floor(p.z());
-    u = u*u*(3-2*u);
-    v = v*v*(3-2*v);
-    w = w*w*(3-2*w);
-
     int i = floor(p.x());
     int j = floor(p.y());
     int k = floor(p.z());
-    float c[2][2][2];
+    vec3 c[2][2][2];
     for (int di=0; di < 2; di++)
       for (int dj=0; dj < 2; dj++)
         for (int dk=0; dk < 2; dk++)
-          c[di][dj][dk] = ranfloat[
+          c[di][dj][dk] = ranvec[
                                    perm_x[(i+di) & 255] ^
                                    perm_y[(j+dj) & 255] ^
                                    perm_z[(k+dk) & 255]
                                    ];
-    return trilinear_interp(c, u, v, w);
+    return perlin_interp(c, u, v, w);
   }
-  static float *ranfloat;
+  static vec3 *ranvec;
   static int *perm_x;
   static int *perm_y;
   static int *perm_z;
 };
 
-static float* perlin_generate() {
-  float * p = new float[256];
-  for (int i = 0; i < 256; ++i)
-    p[i] = random_double();
+
+static vec3* perlin_generate() {
+  vec3 *p = new vec3[256];
+  for (int i = 0; i < 256; ++i) {
+    double x_random = 2*random_double() - 1;
+    double y_random = 2*random_double() - 1;
+    double z_random = 2*random_double() - 1;
+    p[i] = unit_vector(vec3(x_random, y_random, z_random));
+  }
   return p;
 }
 
@@ -71,7 +78,7 @@ static int* perlin_generate_perm() {
   return p;
 }
 
-float *perlin::ranfloat = perlin_generate();
+vec3 *perlin::ranvec = perlin_generate();
 
 int *perlin::perm_x = perlin_generate_perm();
 int *perlin::perm_y = perlin_generate_perm();
