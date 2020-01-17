688b46b71df5893d1e41dbb1d0f5329d7b2870df
David Crook <idcrook@users.noreply.github.com>
2020-01-17 11:59:43 -0700

    ch7: add first emissive material

--- TheNextWeek/src/material.h
+++ TheNextWeek/src/material.h
@@ -60,6 +60,11 @@ public:
   virtual bool scatter(
                        const ray& r_in, const hit_record& rec, vec3& attenuation,
                        ray& scattered) const = 0;
+  // not a pure virtual function (base class returns black)
+  virtual vec3 emitted(float u, float v, const vec3& p) const {
+    (void)u; (void)v; (void)p;
+    return vec3(0,0,0);
+  }
 };
 
 
@@ -143,5 +148,20 @@ public:
   float ref_idx;
 };
 
+
+// add an emissive material
+class diffuse_light : public material {
+    public:
+        diffuse_light(texture *a) : emit(a) {}
+        virtual bool scatter(const ray& r_in, const hit_record& rec,
+            vec3& attenuation, ray& scattered) const {
+          (void)r_in; (void)rec; (void)attenuation; (void)scattered;
+          return false;
+        }
+        virtual vec3 emitted(float u, float v, const vec3& p) const {
+            return emit->value(u, v, p);
+        }
+        texture *emit;
+};
 
 #endif
