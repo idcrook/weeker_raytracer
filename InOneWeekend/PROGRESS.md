22f99e8a02d3ec278cfcfaf15339a1d5d3edc1ef
David Crook <idcrook@users.noreply.github.com>
2020-01-17 11:47:16 -0700

    copied In One Weekend code
     - commit history found in beginning of this repo

---
+++ InOneWeekend/PROGRESS.md
@@ -0,0 +1,18 @@
+# ray tracing in one weekend book
+
+
+https://raytracing.github.io/books/RayTracingInOneWeekend.html
+
+# chapter 2
+
+created makefile and source tree
+
+```
+make
+./build/apps/program > ch2.ppm
+```
+
+- `Makefile` derived from https://www.partow.net/programming/makefile/index.html
+
+
+# chapter 3
---
+++ InOneWeekend/src/hittable.h
@@ -0,0 +1,21 @@
+#ifndef HITTABLEH
+#define HITTABLEH
+
+#include "ray.h"
+
+class material;
+
+struct hit_record {
+    float t;
+    vec3 p;
+    vec3 normal;
+    material *mat_ptr;
+};
+
+class hittable  {
+    public:
+        virtual bool hit(
+            const ray& r, float t_min, float t_max, hit_record& rec) const = 0;
+};
+
+#endif
