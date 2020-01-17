6598ede88606db247789a9d7af345109d516f754
David Crook <idcrook@users.noreply.github.com>
2020-01-17 11:01:12 -0700

    re-org file hierarchy

---
+++ TheNextWeek/src/texture.h
@@ -0,0 +1,50 @@
+#ifndef TEXTUREH
+#define TEXTUREH
+
+#include "perlin.h"
+#include "vec3.h"
+
+class texture {
+public:
+  virtual vec3 value(float u, float v, const vec3& p) const = 0;
+};
+
+class constant_texture : public texture {
+public:
+  constant_texture() {}
+  constant_texture(vec3 c) : color(c) {}
+  virtual vec3 value(float u, float v, const vec3& p) const {
+    (void)u; (void)v; (void)p;
+    return color;
+  }
+  vec3 color;
+};
+
+class checker_texture : public texture {
+public:
+  checker_texture() {}
+  checker_texture(texture *t0, texture *t1): even(t0), odd(t1) {}
+  virtual vec3 value(float u, float v, const vec3& p) const {
+    float sines = sin(10*p.x())*sin(10*p.y())*sin(10*p.z());
+    if (sines < 0)
+      return odd->value(u, v, p);
+    else
+      return even->value(u, v, p);
+  }
+  texture *even;
+  texture *odd;
+};
+
+class noise_texture : public texture {
+public:
+  noise_texture() {}
+  noise_texture(float sc) : scale(sc) {}
+  virtual vec3 value(float u, float v, const vec3& p) const {
+    (void)u; (void)v;
+    return vec3(1,1,1) * 0.5 * (1 + sin(scale*p.z() + 10*noise.turb(p)));
+  }
+  perlin noise;
+  float scale;
+};
+
+#endif
