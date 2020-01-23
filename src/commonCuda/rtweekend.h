e2edc9df1891b8d3b359236dbaa227294871ff1b
David Crook <idcrook@users.noreply.github.com>
2020-01-22 22:25:22 -0700

    comment out unused includes and utility functions

--- src/commonCuda/rtweekend.h
+++ src/commonCuda/rtweekend.h
@@ -2,25 +2,25 @@
 #define RTWEEKEND_H
 
 
-#include <cstdlib>
-#include <limits>
-#include <cmath>
+// #include <cstdlib>
+// #include <limits>
+// #include <cmath>
 
-#include <functional>
-#include <random>
+// #include <functional>
+// #include <random>
 
-// Utility Functions
+// // Utility Functions
 
-inline double ffmin(double a, double b) { return a <= b ? a : b; }
-inline double ffmax(double a, double b) { return a >= b ? a : b; }
+// inline double ffmin(double a, double b) { return a <= b ? a : b; }
+// inline double ffmax(double a, double b) { return a >= b ? a : b; }
 
-inline double random_double() {
-    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
-    static std::mt19937 generator;
-    static std::function<double()> rand_generator =
-      std::bind(distribution, generator);
-    return rand_generator();
-  }
+// inline double random_double() {
+//     static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+//     static std::mt19937 generator;
+//     static std::function<double()> rand_generator =
+//       std::bind(distribution, generator);
+//     return rand_generator();
+//   }
 
 // Common Headers
 
