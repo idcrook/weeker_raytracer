dc7dabcf44333d4bce93c8a4c0cccaf2a9755a29
David Crook <idcrook@users.noreply.github.com>
2020-01-22 20:52:57 -0700

    ch4: hit sphere

--- src/InOneWeekendCuda/main.cu
+++ src/InOneWeekendCuda/main.cu
@@ -23,7 +23,18 @@ void check_cuda(cudaError_t result, char const *const func, const char *const fi
   }
 }
 
+__device__ bool hit_sphere(const vec3& center, float radius, const ray& r) {
+  vec3 oc = r.origin() - center;
+  float a = dot(r.direction(), r.direction());
+  float b = 2.0f * dot(oc, r.direction());
+  float c = dot(oc, oc) - radius*radius;
+  float discriminant = b*b - 4.0f*a*c;
+  return (discriminant > 0.0f);
+}
+
 __device__ vec3 color(const ray& r) {
+  if (hit_sphere(vec3(0,0,-1), 0.5, r))
+    return vec3(1,0,0);
   vec3 unit_direction = unit_vector(r.direction());
   float t = 0.5f*(unit_direction.y() + 1.0f);
   return (1.0f-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
