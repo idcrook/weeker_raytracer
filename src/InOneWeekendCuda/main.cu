157ee900c5aa9a516f48d7e3a1a16b7664a51c1a
David Crook <idcrook@users.noreply.github.com>
2020-01-22 22:24:37 -0700

    ch10: add positionable camera

--- src/InOneWeekendCuda/main.cu
+++ src/InOneWeekendCuda/main.cu
@@ -89,7 +89,7 @@ __global__ void render(vec3 *fb, int max_x, int max_y, int ns, camera **cam, hit
   fb[pixel_index] = col;
 }
 
-__global__ void create_world(hittable **d_list, hittable **d_world, camera **d_camera) {
+__global__ void create_world(hittable **d_list, hittable **d_world, camera **d_camera, int nx, int ny) {
   if (threadIdx.x == 0 && blockIdx.x == 0) {
     d_list[0] = new sphere(vec3(0,0,-1), 0.5,
                            new lambertian(vec3(0.1, 0.2, 0.5)));
@@ -102,7 +102,11 @@ __global__ void create_world(hittable **d_list, hittable **d_world, camera **d_c
     d_list[4] = new sphere(vec3(-1,0,-1), -0.45,
                            new dielectric(1.5));
     *d_world = new hittable_list(d_list,5);
-    *d_camera = new camera();
+    *d_camera   = new camera(vec3(-2,2,1),
+                             vec3(0,0,-1),
+                             vec3(0,1,0),
+                             20.0,
+                             float(nx)/float(ny));
   }
 }
 
@@ -143,7 +147,7 @@ int main() {
   checkCudaErrors(cudaMalloc((void **)&d_world, sizeof(hittable *)));
   camera **d_camera;
   checkCudaErrors(cudaMalloc((void **)&d_camera, sizeof(camera *)));
-  create_world<<<1,1>>>(d_list,d_world,d_camera);
+  create_world<<<1,1>>>(d_list, d_world, d_camera, nx, ny);
   checkCudaErrors(cudaGetLastError());
   checkCudaErrors(cudaDeviceSynchronize());
 
--- src/commonCuda/camera.h
+++ src/commonCuda/camera.h
@@ -1,15 +1,23 @@
 #ifndef CAMERA_H    /* -*- cuda -*- */
 #define CAMERA_H
 
-#include "commonCuda/ray.h"
+#include "commonCuda/rtweekend.h"
 
 class camera {
 public:
-  __device__ camera() {
-    lower_left_corner = vec3(-2.0, -1.0, -1.0);
-    horizontal = vec3(4.0, 0.0, 0.0);
-    vertical = vec3(0.0, 2.0, 0.0);
-    origin = vec3(0.0, 0.0, 0.0);
+  __device__ camera(vec3 lookfrom, vec3 lookat, vec3 vup, float vfov, float aspect) {
+    // vfov is top to bottom in degrees
+    vec3 u, v, w;
+    float theta = vfov*M_PI/180;
+    float half_height = tan(theta/2);
+    float half_width = aspect * half_height;
+    origin = lookfrom;
+    w = unit_vector(lookfrom - lookat);
+    u = unit_vector(cross(vup, w));
+    v = cross(w, u);
+    lower_left_corner = origin - half_width*u -half_height*v - w;
+    horizontal = 2*half_width*u;
+    vertical = 2*half_height*v;
   }
   __device__ ray get_ray(float u, float v) { return ray(origin, lower_left_corner + u*horizontal + v*vertical - origin); }
 
