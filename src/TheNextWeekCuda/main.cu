6709e821b8a8e1479fae1b4118d727cc2a5fa9df
David Crook <idcrook@users.noreply.github.com>
2020-01-27 16:51:58 -0700

    have types all match so memory cleanup is correct

--- src/TheNextWeekCuda/main.cu
+++ src/TheNextWeekCuda/main.cu
@@ -101,8 +101,12 @@ __global__ void render(vec3 *fb, int max_x, int max_y, int ns, camera **cam, hit
 __global__ void create_world(hittable **d_list, hittable **d_world, camera **d_camera, int nx, int ny, curandState *rand_state) {
   if (threadIdx.x == 0 && blockIdx.x == 0) {
     curandState local_rand_state = *rand_state;
-    d_list[0] = new sphere(vec3(0,-1000.0,-1), 1000,
-                           new lambertian(vec3(0.5, 0.5, 0.5)));
+    // d_list[0] = new sphere(vec3(0,-1000.0,-1), 1000,
+    //                        new lambertian(vec3(0.5, 0.5, 0.5)));
+    d_list[0] = new moving_sphere(vec3(0,-1000.0,-1), vec3(0,-1000.0,-1),
+                                  0.f, 1.f,
+                                  1000,
+                                  new lambertian(vec3(0.5, 0.5, 0.5)));
     int i = 1;
     for(int a = -11; a < 11; a++) {
       for(int b = -11; b < 11; b++) {
@@ -117,25 +121,33 @@ __global__ void create_world(hittable **d_list, hittable **d_world, camera **d_c
                                           new lambertian(vec3(RND*RND, RND*RND, RND*RND)));
         }
         else if(choose_mat < 0.95f) {
-          d_list[i++] = new sphere(center, 0.2,
+          // d_list[i++] = new sphere(center, 0.2,
+          //                          new metal(vec3(0.5f*(1.0f+RND), 0.5f*(1.0f+RND), 0.5f*(1.0f+RND)), 0.5f*RND));
+          d_list[i++] = new moving_sphere(center, center,
+                                   0.f, 1.f,
+                                   0.2,
                                    new metal(vec3(0.5f*(1.0f+RND), 0.5f*(1.0f+RND), 0.5f*(1.0f+RND)), 0.5f*RND));
         }
         else {
-          d_list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+          //d_list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+          d_list[i++] = new moving_sphere(center, center, 0.f, 1.f, 0.2, new dielectric(1.5));
         }
       }
     }
-    d_list[i++] = new sphere(vec3(0, 1,0),  1.0, new dielectric(1.5));
-    d_list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
-    d_list[i++] = new sphere(vec3(4, 1, 0),  1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+    // d_list[i++] = new sphere(vec3(0, 1,0),  1.0, new dielectric(1.5));
+    // d_list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+    // d_list[i++] = new sphere(vec3(4, 1, 0),  1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+    d_list[i++] = new moving_sphere(vec3(0, 1,0),  vec3(0, 1,0),   0.f, 1.f, 1.0, new dielectric(1.5));
+    d_list[i++] = new moving_sphere(vec3(-4, 1, 0),vec3(-4, 1, 0), 0.f, 1.f, 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+    d_list[i++] = new moving_sphere(vec3(4, 1, 0), vec3(4, 1, 0),  0.f, 1.f, 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
     *rand_state = local_rand_state;
     *d_world  = new hittable_list(d_list, 22*22+1+3);
 
     vec3 lookfrom(13,2,3);
     vec3 lookat(0,0,0);
     float dist_to_focus = 10.0; (lookfrom-lookat).length();
-    //float aperture = 0.1;
-    float aperture = 0.0;
+    //float aperture = 0.1f;
+    float aperture = 0.0f;
     *d_camera   = new camera(lookfrom,
                              lookat,
                              vec3(0,1,0),
@@ -149,7 +161,8 @@ __global__ void create_world(hittable **d_list, hittable **d_world, camera **d_c
 
 __global__ void free_world(hittable **d_list, hittable **d_world, camera **d_camera) {
   for(int i=0; i < 22*22+1+3; i++) {
-    delete ((sphere *)d_list[i])->mat_ptr;
+    //delete ((sphere *)d_list[i])->mat_ptr;
+    delete ((moving_sphere *)d_list[i])->mat_ptr;
     delete d_list[i];
   }
   delete *d_world;
--- src/TheNextWeekCuda/material.cuh
+++ src/TheNextWeekCuda/material.cuh
@@ -51,7 +51,6 @@ public:
   __device__ lambertian(const vec3& a) : albedo(a) {}
   __device__ virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered, curandState *local_rand_state) const  {
     vec3 target = rec.p + rec.normal + random_in_unit_sphere(local_rand_state);
-    //scattered = ray(rec.p, target-rec.p);
     scattered = ray(rec.p, target-rec.p, r_in.time());
     attenuation = albedo;
     return true;
@@ -65,7 +64,7 @@ public:
   __device__ metal(const vec3& a, float f) : albedo(a) { if (f < 1) fuzz = f; else fuzz = 1; }
   __device__ virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered, curandState *local_rand_state) const  {
     vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-    scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere(local_rand_state));
+    scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere(local_rand_state), r_in.time());
     attenuation = albedo;
     return (dot(scattered.direction(), rec.normal) > 0.0f);
   }
@@ -104,9 +103,9 @@ public:
     else
       reflect_prob = 1.0f;
     if (curand_uniform(local_rand_state) < reflect_prob)
-      scattered = ray(rec.p, reflected);
+      scattered = ray(rec.p, reflected, r_in.time());
     else
-      scattered = ray(rec.p, refracted);
+      scattered = ray(rec.p, refracted, r_in.time());
     return true;
   }
 
