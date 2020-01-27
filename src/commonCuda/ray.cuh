e37ac7f480e39fe59cf9131a36c7d543d3d4f809
David Crook <idcrook@users.noreply.github.com>
2020-01-27 15:59:41 -0700

    ch2 ray with timestamp

--- src/commonCuda/ray.cuh
+++ src/commonCuda/ray.cuh
@@ -7,13 +7,15 @@ class ray
 {
 public:
   __device__ ray() {}
-  __device__ ray(const vec3& a, const vec3& b) { A = a; B = b; }
+  __device__ ray(const vec3& a, const vec3& b, float ti = 0.f) { A = a; B = b; _time = ti;}
   __device__ vec3 origin() const       { return A; }
   __device__ vec3 direction() const    { return B; }
+  __device__ float time() const { return _time ; }
   __device__ vec3 point_at_parameter(float t) const { return A + t*B; }
 
   vec3 A;
   vec3 B;
+  float _time;
 };
 
 #endif
