d8a9ad49e63ca21a6191950dccbd9a39a8ce0248
David Crook <idcrook@users.noreply.github.com>
2020-01-28 21:46:40 -0700

    optix: add metal material

--- src/OptiX/InOneWeekend/scene/ioScene.h
+++ src/OptiX/InOneWeekend/scene/ioScene.h
@@ -1,6 +1,8 @@
 #ifndef IO_SCENE_H
 #define IO_SCENE_H
 
+#include <vector>
+
 #include <optix.h>
 #include <optixu/optixpp.h>
 
@@ -11,6 +13,7 @@
 #include "geometry/ioSphere.h"
 #include "material/ioNormalMaterial.h"
 #include "material/ioLambertianMaterial.h"
+#include "material/ioMetalMaterial.h"
 
 class ioScene
 {
@@ -19,41 +22,47 @@ public:
 
   void init(optix::Context& context)
     {
-      // Geometry
-      geometryList.push_back(new ioSphere(
-                               0.0f, 0.0f, -1.0f,
-                               0.5f
-                               ));
+      // Base Sphere
       geometryList.push_back(new ioSphere(
                                0.0, -100.5, -1.0f,
                                100.0f
                                ));
-      geometryList[0]->init(context);
-      geometryList[1]->init(context);
+      materialList.push_back(new ioLambertianMaterial(0.8f, 0.5f, 0.3f));
 
-      // Material
-      materialList.push_back(new ioNormalMaterial());
-      //materialList.push_back(new ioLambertianMaterial(0.7f, 0.5f, 0.0f));
-      materialList.push_back(new ioLambertianMaterial(0.5f, 0.5f, 0.5f));
-      materialList[0]->init(context);
-      materialList[1]->init(context);
+      // Metal Spheres
+      geometryList.push_back(new ioSphere(-1.0f, 0.0f, -1.0f, 0.2f));
+      materialList.push_back(new ioMetalMaterial(0.9f, 0.9f, 0.9f, 1.0f));
+      geometryList.push_back(new ioSphere(-0.5f, 0.0f, -1.0f, 0.2f));
+      materialList.push_back(new ioMetalMaterial(0.9f, 0.9f, 0.9f, 0.75f));
+      geometryList.push_back(new ioSphere( 0.0f, 0.0f, -1.0f, 0.2f));
+      materialList.push_back(new ioMetalMaterial(0.9f, 0.9f, 0.9f, 0.5f));
+      geometryList.push_back(new ioSphere( 0.5f, 0.0f, -1.0f, 0.2f));
+      materialList.push_back(new ioMetalMaterial(0.9f, 0.9f, 0.9f, 0.25f));
+      geometryList.push_back(new ioSphere( 1.0f, 0.0f, -1.0f, 0.2f));
+      materialList.push_back(new ioMetalMaterial(0.9f, 0.9f, 0.9f, 0.0f));
+
+      // init all geometry
+      for(int i = 0; i < geometryList.size(); i++)
+        geometryList[i]->init(context);
+      // init all materials
+      for(int i = 0; i < geometryList.size(); i++)
+        materialList[i]->init(context);
 
       // GeometryInstance
-      //  Normal Sphere
-      geoInstList.push_back(ioGeometryInstance());
-      geoInstList[0].init(context);
-      geoInstList[0].setGeometry(*geometryList[0]);
-      geoInstList[0].setMaterial(*materialList[0]);
-      // Lambertian Sphere
-      geoInstList.push_back(ioGeometryInstance());
-      geoInstList[1].init(context);
-      geoInstList[1].setGeometry(*geometryList[1]);
-      geoInstList[1].setMaterial(*materialList[1]);
+      geoInstList.resize(geometryList.size());
+      // Taking advantage of geometryList.size == materialList.size
+      for (int i = 0; i < geoInstList.size(); i++)
+      {
+        geoInstList[i] = ioGeometryInstance();
+        geoInstList[i].init(context);
+        geoInstList[i].setGeometry(*geometryList[i]);
+        geoInstList[i].setMaterial(*materialList[i]);
+      }
 
       // World & Acceleration
       geometryGroup.init(context);
-      geometryGroup.addChild(geoInstList[0]);
-      geometryGroup.addChild(geoInstList[1]);
+      for (int i = 0; i < geoInstList.size(); i++)
+        geometryGroup.addChild(geoInstList[i]);
 
       // Setting World Variable
       context["sysWorld"]->set(geometryGroup.get());
