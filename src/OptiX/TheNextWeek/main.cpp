9e4149ccbf6489f960c710f389aec154db432384
David Crook <idcrook@users.noreply.github.com>
2020-02-05 16:28:43 -0700

    command line handling: parse/check some options, add help message

--- src/OptiX/TheNextWeek/main.cpp
+++ src/OptiX/TheNextWeek/main.cpp
@@ -1,10 +1,11 @@
-// std
+
 #include <iostream>
+#include <stdexcept>
 #include <chrono>
 
 // Image I/O
 // define STB_IMAGE*_IMPLEMENTATION-s only once (e.g. in .cpp file)
-#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION1
 // #define STB_IMAGE_WRITE_IMPLEMENTATION // not yet used
 #include "../external/rtw_stb_image.h"
 
@@ -14,21 +15,85 @@
 
 // Executive Director
 #include "Director.h"
+// Parse command line arguments and options
+#include "InputParser.h"
+
+#define Nscene_MAX  (3)
+#define Ns_MAX  (1024*10)
 
 int main(int argc, char* argv[])
 {
-  Director optixSingleton = Director();
+    int exit_code = EXIT_SUCCESS;
 
-  auto start = std::chrono::system_clock::now();
-  //optixSingleton.init(1200, 600);
-  optixSingleton.init(560, 560); // cornell box resolution
-  optixSingleton.createScene();
-  optixSingleton.renderFrame();
-  auto stop = std::chrono::system_clock::now();
-  auto time_seconds = std::chrono::duration<float>(stop - start).count();
-  std::cerr << "Took " << time_seconds << " seconds.\n";
+    int Nscene = 0;
+    int Ns = 1024;
 
-  optixSingleton.printPPM();
+    InputParser cl_input(argc, argv);
+    if(cl_input.cmdOptionExists("-h")){
+        std::cout << std::endl <<  " HELP - " << argv[0] << std::endl;
+        std::cout << R"(
+    -s N           Scene Selection number N (N: 0, 1, 2, etc.)
+    -n N           Sample each pixel N times (N: 1, 2, etc.)
+    -dx Nx         (TBD) Output image width (x dimension)
+    -dy Ny         (TBD) Output image height (y dimension)
 
-  optixSingleton.destroy();
+    -h             This help message.
+    -v             (TBD) Verbose
+    -g             (TBD) Debug
+
+)";
+        std::exit( exit_code );
+    }
+    const std::string &sceneNumber = cl_input.getCmdOption("-s");
+    try {
+        if (!sceneNumber.empty()){
+            std::size_t pos;
+            int x = std::stoi(sceneNumber, &pos);
+            if (x >= Nscene and x <= Nscene_MAX) {
+                Nscene = x;
+            } else {
+                std::cerr << "WARNING: Scene number " << x << " out of range. Maximum scene number: " << Nscene_MAX << " "
+                          << "Using a value of " << Nscene <<std::endl;
+            }
+        }
+    } catch (std::invalid_argument const &ex) {
+        std::cerr << "Invalid scene number: " << sceneNumber << std::endl;
+    }
+
+    const std::string &numberOfSamples = cl_input.getCmdOption("-n");
+    try {
+        if (!numberOfSamples.empty()){
+            std::size_t pos;
+            int x = std::stoi(numberOfSamples, &pos);
+            if ( (x > 0) && (x <= Ns_MAX))  {
+                Ns = x;
+            } else {
+                std::cerr << "WARNING: Number of samples " << x << " is out of range. ";
+                if (x > Ns_MAX) {
+                    Ns = Ns_MAX;
+                }
+                std::cerr << "Using a value of " << Ns << std::endl;
+            }
+        }
+    } catch (std::invalid_argument const &ex) {
+        std::cerr << "Invalid scene number: " << sceneNumber << std::endl;
+    }
+
+    Director optixSingleton = Director();
+
+
+    auto start = std::chrono::system_clock::now();
+    //optixSingleton.init(1200, 600);
+    optixSingleton.init(560, 560); // cornell box resolution
+    std::cerr << "Scene number selected: " << Nscene << std::endl;
+    std::cerr << "Number of rays sent per pixel: " << Ns << std::endl;
+    optixSingleton.createScene();
+    optixSingleton.renderFrame();
+    auto stop = std::chrono::system_clock::now();
+    auto time_seconds = std::chrono::duration<float>(stop - start).count();
+    std::cerr << "Took " << time_seconds << " seconds." << std::endl;
+
+    optixSingleton.printPPM();
+
+    optixSingleton.destroy();
 }
